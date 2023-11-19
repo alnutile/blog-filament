dd4c1bc14665d1f6eb78e04a3c4b38aa58f02180
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2023-11-18 21:04:41 -0500

    add analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,4 +18,16 @@
     <body class="font-sans antialiased">
         @inertia
     </body>
+
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-36FWYFE06E"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-36FWYFE06E');
+    </script>
+
 </html>
